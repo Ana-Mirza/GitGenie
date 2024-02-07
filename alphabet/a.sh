785c98021f8ef58c2d166d5c44a21c3061408af5
Ana-Mirza <anamirza2002@gmail.com>
2024-02-07 20:19:19 +0200

    I have awakenend a..

--- alphabet/a.sh
+++ alphabet/a.sh
@@ -1,1 +1,48 @@
 # This is not done yet, boy!
+
+echo "I will conquer this!" > file.txt
+rm file.txt
+
+# Commit A..
+git add .
+git commit -m "The idea has merit."
+
+# /..
+git commit --amend --no-edit --date "22/01/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "23/01/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "24/01/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "25/01/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "26/01/24 20:19:19 +0200"
+git push -f origin main
+
+# ../-..
+
+git commit --amend --no-edit --date "29/01/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "31/01/24 20:19:19 +0200"
+git push -f origin main
+
+# ../-\..
+git commit --amend --no-edit --date "5/02/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "6/02/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "7/02/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "8/02/24 20:19:19 +0200"
+git push -f origin main
+
+git commit --amend --no-edit --date "9/02/24 20:19:19 +0200"
+git push -f origin main
