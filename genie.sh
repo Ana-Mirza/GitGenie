a397a4b594cc4e760e78b078cd367e08abab2af4
Ana Mirza <anamirza2002@gmail.com>
2025-01-22 02:03:07 +0200

    Well, there’s this girl …

--- genie.sh
+++ genie.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+# Input phrase
 echo "Yo, Rugman! Haven't Seen You In A Few Millennia. Give Me Some Tassel."
 read phrase
 
-# Enter start date for the drawing
-echo "When do you want to go in time?"
+# Start date for the drawing
+echo "How far do you want to go in time?"
 read date
 
 echo "Let's make some magic!"
 
-# Create commit
+# Commit the drawing
 git add .
 git commit -m "The idea has merit."
 git push
 
+# Farewell
 echo "You Ain't Never Had A Friend Like Me!"
